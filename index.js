bbfdf016f87fc93a72e8842ed6a133bb65ceccf2
danilocutrim <danilocutrim.dc@gmail.com>
2019-02-26 13:55:39 -0300

    mudança de parametros docker

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ const rotaAccounts = require('./routes/accounts')(app);
 const rotaTransactions = require('./routes/get_transactions')(app);
 const rotaTransactionsCategories = require('./routes/get_tran_cat')(app);
 
-const PORT = process.env.PORT || 3000;
-const HOST = '127.0.0.1';
+const PORT = 8080;
+const HOST = '0.0.0.0';
 
 app.listen(PORT, HOST);
 console.log(`Rodando em  http://${HOST}:${PORT}`);
